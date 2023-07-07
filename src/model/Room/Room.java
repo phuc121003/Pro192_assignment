9347d4f6ac19fc8d62886bacaf7ce6a0a827dbc6
Tuanngp <121474156+Tuanngp@users.noreply.github.com>
2023-07-07 12:18:00 +0700

    Add files via upload

---
+++ src/model/Room/Room.java
@@ -0,0 +1,57 @@
+package model.Room;
+
+public class Room {
+
+    private String roomID;
+    private String roomType;
+    private float price;
+    private boolean isRented = false;
+
+    public Room() {
+    }
+    
+    public Room(String roomID, String roomType, float price, boolean isRented) {
+        this.roomID = roomID;
+        this.roomType = roomType;
+        this.price = price;
+        this.isRented = isRented;
+    }
+
+    public String getRoomID() {
+        return roomID;
+    }
+
+    public void setRoomID(String roomID) {
+        this.roomID = roomID;
+    }
+
+    public String getRoomType() {
+        return roomType;
+    }
+
+    public void setRoomType(String roomType) {
+        this.roomType = roomType;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public boolean isIsRented() {
+        return isRented;
+    }
+
+    public void setIsRented(boolean isRented) {
+        this.isRented = isRented;
+    }
+
+    @Override
+    public String toString() {
+        return "|" + roomID + "\t" + roomType + "\t" + price + "\t" + isRented + "|";
+    }
+
+}
